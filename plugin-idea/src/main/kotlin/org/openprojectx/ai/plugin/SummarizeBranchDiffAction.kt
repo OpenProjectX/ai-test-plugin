e93ba8eb90b75badde0d1c83fec5e851edd240e5
Idddd <956020859@qq.com>
2026-03-19 16:34:47 +0800

    fix: register summary action for vcs diff toolbar contexts

--- plugin-idea/src/main/kotlin/org/openprojectx/ai/plugin/SummarizeBranchDiffAction.kt
+++ plugin-idea/src/main/kotlin/org/openprojectx/ai/plugin/SummarizeBranchDiffAction.kt
@@ -1,9 +1,9 @@
 package org.openprojectx.ai.plugin
 
 import com.intellij.icons.AllIcons
-import com.intellij.openapi.actionSystem.AnAction
 import com.intellij.openapi.actionSystem.AnActionEvent
 import com.intellij.openapi.actionSystem.CommonDataKeys
+import com.intellij.openapi.actionSystem.DumbAwareAction
 import com.intellij.openapi.application.ApplicationManager
 import com.intellij.openapi.progress.ProgressIndicator
 import com.intellij.openapi.progress.ProgressManager
@@ -11,7 +11,7 @@ import com.intellij.openapi.progress.Task
 import com.intellij.openapi.ui.Messages
 import com.intellij.vcs.log.VcsLogDataKeys
 
-class SummarizeBranchDiffAction : AnAction(
+class SummarizeBranchDiffAction : DumbAwareAction(
     null,
     "Summarize Branch Differences",
     AllIcons.Actions.Refresh
